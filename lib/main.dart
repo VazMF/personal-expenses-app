5e8f22b64837d39fdcb67a80a594dfd007f686b2
vaz_mf <vazfernandam@gmail.com>
2021-05-06 15:18:18 -0300

    Adicionando dados da transaçao no Card

--- lib/main.dart
+++ lib/main.dart
@@ -16,19 +16,14 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-      id: 't1', 
-      title: 'New shoes', 
-      amount: 69.90, 
-      date: DateTime.now()
-    ),
+        id: 't1', title: 'New shoes', amount: 69.90, date: DateTime.now()),
     Transaction(
-      id: 't2', 
-      title: 'Weekly groceries', 
-      amount: 16.50, 
-      date: DateTime.now()
-    )
+        id: 't2',
+        title: 'Weekly groceries',
+        amount: 16.50,
+        date: DateTime.now())
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,11 +41,23 @@ class MyHomePage extends StatelessWidget {
                 child: Text('CHART', style: TextStyle(color: Colors.white))),
             elevation: 4,
           ),
-          Column(children: transactions.map((tx) {
-            return Card(
-              child: Text(tx.title),
-            );
-          }).toList(),),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(children: <Widget>[
+                      Text(tx.title),
+                      Text(tx.date.toString())
+                    ],),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
