40347bb45453b840410baca0e9cae925a14b494e
vaz_mf <vazfernandam@gmail.com>
2021-05-06 13:20:31 -0300

    Utilizando colunas e Adicionando transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,43 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'New shoes', 
+      amount: 69.90, 
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Weekly groceries', 
+      amount: 16.50, 
+      date: DateTime.now()
+    )
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget playground'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+                width: double.infinity,
+                child: Text('CHART', style: TextStyle(color: Colors.white))),
+            elevation: 4,
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('LIST OF TX', style: TextStyle(color: Colors.white),)
+          ),
+        ],
       ),
     );
   }
