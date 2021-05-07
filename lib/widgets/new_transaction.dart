c583372a3be82ee0a5942ca7af6269cd4fc1acbd
vaz_mf <vazfernandam@gmail.com>
2021-05-07 13:14:29 -0300

    Melhorando os inputs e outputs

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,37 +7,51 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return; // return stops the function execution
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextField(
-                  decoration: InputDecoration(labelText: 'Title'),
-                  controller: titleController,
-                  // onChanged: (value) => titleInput = value,
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Amount'), 
-                  keyboardType: TextInputType.number,
-                  controller: amountController,
-                  // onChanged: (value) => amountInput = value,
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                     foregroundColor: MaterialStateProperty.all(Colors.purple)
-                  ),
-                  child: Text('Add Transaction'), 
-                  onPressed: () {
-                     addTx(titleController.text, double.parse(amountController.text));
-                  },
-                ),
-              ],),
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (value) => titleInput = value,
             ),
-          );
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              controller: amountController,
+              // onChanged: (value) => amountInput = value,
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.purple)),
+              child: Text('Add Transaction'),
+              onPressed: submitData,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
