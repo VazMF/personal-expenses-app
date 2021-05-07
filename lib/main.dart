bab00bb0bc9d1187213a322d8728322debe2928b
vaz_mf <vazfernandam@gmail.com>
2021-05-07 09:17:50 -0300

    Addwidgets de input

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,40 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
+            ),
+          ),
           Card(
-            color: Colors.blue,
+            elevation: 5,
             child: Container(
-                width: double.infinity,
-                child: Text('CHART', style: TextStyle(color: Colors.white))),
-            elevation: 4,
+              padding: EdgeInsets.all(10),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title')
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'), 
+                  keyboardType: TextInputType.number,
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                     foregroundColor: MaterialStateProperty.all(Colors.purple)
+                  ),
+                  child: Text('Add Transaction'), 
+                  onPressed: () {},
+                ),
+              ],),
+            ),
           ),
           Column(
             children: transactions.map((tx) {
